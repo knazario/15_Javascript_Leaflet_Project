59a915ee82e94687cf418a16de93f849aa41422d
Kevin Nazario <kevin.l.nazario@gmail.com>
2024-03-18 20:13:11 -0700

    Update project file and readme

--- Leaflet_Part_1/static/js/logic.js
+++ Leaflet_Part_1/static/js/logic.js
@@ -1,9 +1,41 @@
-// Perform an API call
-// Pulling all 1.0 and larger earthquakes in the last 7 days 
-const url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_week.geojson';
 
+// Setting URL to all 1.0 and larger earthquakes in the last 7 days 
+const url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_week.geojson';
+// Perform an API call and load in data using d3. Send data to createMarkers function
 d3.json(url).then(createMarkers);
- 
+
+//function to create overlay of earthquake data
+function createMarkers(data){
+    //create empty array for populating earthquake markers
+    let earthquakes = [];
+
+    for (i = 0; i < data.features.length; i++){
+        // loop through all data features (earthquakes), assign depth and lat/long to variables
+        earthquake = data.features[i];
+        let depth = earthquake.geometry.coordinates[2];
+        let latlng = [earthquake.geometry.coordinates[1], 
+                    earthquake.geometry.coordinates[0]];
+        //add circle polygon for each earthquake with options below
+        earthquakes.push(
+            L.circle(latlng, {
+                radius: earthquake.properties.mag * 15000,  // set radius of circle to magnitude (scaled in meters)
+                fillColor: getColor(depth),     // set fill color based on depth (call getColor function to assign color)
+                color: "#000",                  // set outline to black 
+                weight: .5,                     // set circle outline stroke weight
+                fillOpacity: 1                  // set fill opacity
+            //set pop-up to inclue title (includes mag and location), date, sig level, and depth.
+            }).bindPopup(`<h3>${earthquake.properties.title}</h3><hr>`+         
+                `<p> Date: ${new Date(earthquake.properties.time)}</p>`+
+                `<p> Significance Level (0-1000): ${earthquake.properties.sig}</p>`+
+                `<p> Depth: ${depth} km</p>`)
+         ) ;
+    }
+    let earthquake_layer = L.layerGroup(earthquakes);   // create overlay layer group 
+
+    createMap(earthquake_layer);    // pass layer group to createMap function
+}
+
+//when called, getColor will take the depth provided and return a hex color based on the depth
 function getColor(d) {
     return d >= 90 ? '#d73027' :
            d >= 70  ? '#fc8d59' :
@@ -12,35 +44,7 @@ function getColor(d) {
            d >= 10   ? '#91cf60':
                         '#1a9850';
 }
-
-function createMarkers(data){
-    console.log(data);
-
-    let earthquakes = [];
-
-    for (i = 0; i < data.features.length; i++){
-        earthquake = data.features[i];
-        let depth2 = earthquake.geometry.coordinates[2];
-        let latlng = [earthquake.geometry.coordinates[1], 
-                    earthquake.geometry.coordinates[0]];
-
-        earthquakes.push(
-            L.circle(latlng, {
-                radius: earthquake.properties.mag * 15000,
-                fillColor: getColor(depth2),
-                color: "#000", 
-                weight: .5,
-                fillOpacity: 1
-            }).bindPopup(`<h3>${earthquake.properties.title}</h3><hr>`+
-                `<p> Date: ${new Date(earthquake.properties.time)}</p>`+
-                `<p> Significance Level (0-1000): ${earthquake.properties.sig}</p>`+
-                `<p> Depth: ${depth2} km</p>`)
-         ) ;
-    }
-    let earthquake_layer = L.layerGroup(earthquakes);
-
-    createMap(earthquake_layer);
-}
+// function to create map, overlays, layer controls and legend 
 function createMap(earthquakes){
     // Create the tile layer (background) for map
     let base = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -54,21 +58,22 @@ function createMap(earthquakes){
 
     // Create the map object with options.
     let myMap = L.map("map", {
-    center: [38.5, -96.5],     // Center of World [11.75, 18.05]
+    center: [38.5, -96.5],     // can also use center of world approximation [11.75, 18.05]
     zoom: 5,
-    layers: [base, earthquakes]
+    layers: [base, earthquakes] //base layers to display
     });
 
-    // Create a layer control, and pass it baseMaps and overlayMaps. Add the layer control to the map.
+    // Create a layer control, and pass overlayMaps (don't include base layers as only one). Add the layer control to the map.
     L.control.layers(null, overlayMaps).addTo(myMap);
 
+    // create a legend control object
     var legend = L.control({position: 'bottomright'});
-
+    // create div for legend and provide values when added to map
     legend.onAdd = function (map) {
         var div = L.DomUtil.create('div', 'info legend'),
             depths = [-10, 10, 30, 50, 70, 90],
             labels = [];
-
+        // add legend title
         div.innerHTML = '<h4> Earthquake<br>Depth (km)</h4>';
         // loop through our density intervals and generate a label with a colored square for each interval
         for (var i = 0; i < depths.length; i++) {
@@ -77,9 +82,8 @@ function createMap(earthquakes){
                 depths[i] + (depths[i + 1] ? '&ndash;' + depths[i + 1] + '<br>' : '+');
         }
 
-        return div;
+        return div; //returning div to be placed on map
     };
 
-    legend.addTo(myMap);
+    legend.addTo(myMap);    // add legend to map
 }
-
--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # 15_Javascript_Leaflet_Project
+
+### Project Overview
+This project deliverable involved an interactive map displaying real-time earthquake data and displaying a variety of infomration on about the earthquakes. 
+
+The project was broken up into 2 parts/maps. Part 1 included a base layer map with an overlay of the earthquake markers. Each earthquake marker is a circle where the size is based on the earthquake magnitude and the color is based on the earthquake depth (km). A legend is included for the depth colors and the location, magnitude, significance level, date and depth also are displayed on a pop-up when an earthquake marker is selected. 
+
+Part 2 was completed by adding a tectonic plate layer and an additional base layer (topographic). 
+
+#### Additional Notes
+Part 2 was completed using a local JSON file until another solution can be determined. 
+
+### Data
+For this project, data was accessed/loaded using a D3 JSON promise request from USGS (see Data Reference below).
+The dataset for this map contains all earthquakes from the last 7 days that have a magnitude or 1.0 or larger. 
+
+For part 2, a local JSON file was downlaoded as a workaround for tectonic boundaries from https://github.com/fraxen/tectonicplates
+
+### Data Reference
+Dataset created by the [United States Geological SurveyLinks (USGS)](https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php).
+
+### Code Source
+Additional resources referenced and utilzied for my project code are listed below: 
+
+- Building custom legend</br>
+https://leafletjs.com/examples/choropleth/#custom-legend-control
+
+- Adjusting color for circles based on a function (Adding some Color section)<br/>
+https://leafletjs.com/examples/choropleth/
+
+Leaflet Part 2: (Bonus)
+- Reading in local JSON file <br/> 
+https://api.jquery.com/jQuery.getJSON/
+
